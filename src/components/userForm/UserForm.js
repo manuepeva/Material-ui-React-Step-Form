3f8b7be4ff8f1b60a0f2af2151cf43b3cb390da7
ManuelP <manuepeva@gmail.com>
2020-12-19 11:41:31 -0500

    creating components to display the several steps in the form

--- src/components/userForm/UserForm.js
+++ src/components/userForm/UserForm.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { Component } from 'react'
+import FormUserD from "./formUserD/FormUserD";
+export default class UserForm extends Component {
+    state = {
+        step: 1,
+        firstName: '',
+        lastName: '',
+        email: '',
+        ocupation: '',
+        city: '',
+        bio: ''
+    }
+    // Proceed to next step
+    nextStep = () => {
+        const {step} = this.state
+        this.setState({
+            step: step + 1
+        })
+    }
+    prevStep = () => {
+        const {step} = this.state
+        this.setState({
+            step: step - 1
+        })
+    }
 
-const UserForm = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+    // Handle fields change
+    handleChange = input => e => {
+        this.setState({
+            [input]: e.target.value
+        })
+    }
+
+    render() {
+        const {step} = this.state
+        const {firstName, lastName, email, ocupation, city, bio} = this.state
+        const values = {firstName, lastName, email, ocupation, city, bio}
+        switch(step){
+            case 1:
+            return (
+                <FormUserD 
+                nextStep={this.nextStep}
+                handleChange={this.handleChange}
+                values={values}
+                />
+            )
+            case 2:
+            return  <h1>Form</h1>
+            case 3:
+            return <h1>confirmation</h1>
+            case 4:
+            return <h1>Success</h1>
+
+        }
+    }
 }
-
-export default UserForm
--- src/components/userForm/formUserD/FormUserD.js
+++ src/components/userForm/formUserD/FormUserD.js
@@ -1,11 +1,65 @@
-import React from 'react'
+import React, { Component } from 'react'
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import AppBar from '@material-ui/core/AppBar/AppBar'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button';
+import layout from './stylesFormUD.module.css'
 
-const FormUserD = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+class FormUserD extends Component {
+    continue  =  e => {
+        e.preventDefault()
+        this.props.nextStep()
+    }
+    render() {
+        const {values, handleChange} = this.props
+
+        return (
+            <MuiThemeProvider>
+                <React.Fragment>
+                    <AppBar>
+                        <h1>Enter User Details</h1>
+                    </AppBar>
+                    <br />
+                    <div className="fields">
+                    <TextField 
+                    label="Enter Your First Name"
+                    floatingLabelText="First Name"
+                    onChange={handleChange('First Name')}
+                    defaultValue={values.firstName}
+                    />
+                    <br />
+                    <TextField 
+                    label="Enter Your Last Name"
+                    floatingLabelText="Last Name"
+                    onChange={handleChange('Last Name')}
+                    defaultValue={values.lastName}
+                    />
+                    <br />
+                    <TextField 
+                    label="Enter Your Email"
+                    floatingLabelText="Email"
+                    onChange={handleChange('Email')}
+                    defaultValue={values.email}
+                    />
+                    <br />
+                    <Button 
+                    label="Continue"
+                    primary={true}
+                    style={styles.button}
+                    variant="contained"
+                    onClick={this.continue}
+                    />
+                    </div>
+                </React.Fragment>
+            </MuiThemeProvider>
+        )
+    }
+}
+
+const styles = {
+    button: {
+        margin: 15
+    }
 }
 
 export default FormUserD
